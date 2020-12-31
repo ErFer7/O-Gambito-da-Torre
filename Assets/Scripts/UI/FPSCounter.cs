e957baf43244af20c52e6312d602402fa5e54e62
Eric Fernandes Evaristo <ericfernandes7@gmail.com>
2020-12-30 22:52:25 -0300

    V0.4-inDev

---
+++ Assets/Scripts/UI/FPSCounter.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour
+{
+    private Text text;
+
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.text = Mathf.Floor(1.0F / Time.unscaledDeltaTime) + " FPS";
+    }
+}
